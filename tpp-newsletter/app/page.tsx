78781bb049d33ea81a19501cedc2d7ac543e6e80
Alexander Maat <107603244+alexandermaat99@users.noreply.github.com>
2024-09-08 15:55:07 -0600

    social links

--- tpp-newsletter/app/page.tsx
+++ tpp-newsletter/app/page.tsx
@@ -1,11 +1,13 @@
 import HeroText from "@/components/HeroText";
 import NewsletterForm from "@/components/NewsletterForm";
+import Socials from "@/components/Socials";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center p-10 min-h-screen">
       {<HeroText />}
       {<NewsletterForm />}
+      {<Socials />}
     </main>
   );
 }
